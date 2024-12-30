71df8759060cd7b74c1a0cff5dc64e2a4450f755
Wasir <wasir@koer.no>
2024-12-30 10:04:19 +0600

    Initial Commit

---
+++ iOSJourney/iOSJourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSJourneyApp.swift
+//  iOSJourney
+//
+//  Created by Wasir on 30/12/24.
+//
+
+import SwiftUI
+
+@main
+struct iOSJourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
